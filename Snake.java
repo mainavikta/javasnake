85dccc98a525f846bf3a6fbbfca4d9633c029bce
Victor Maina <mainavikta@gmail.com>
2024-07-22 12:05:05 +0300

    This is my first version of the Snake Game in Java

---
+++ Snake.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        initUI();
+    }
+    
+    private void initUI() {
+        add(new SnakeGame());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
